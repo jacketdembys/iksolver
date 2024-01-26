6ad8e2dbd2748492797a72197cc127c5f0e13a6d
jacketdembys <jacketdembys@yahoo.fr>
2024-01-25 19:17:56 -0600

    running with different joint variations

--- create_multi_experiments.sh
+++ create_multi_experiments.sh
@@ -13,8 +13,8 @@
 #    python ik-solver.py --config-path "$script" &
 #done
 
-python ik-solver.py --config-path train_12.yaml &
-python ik-solver.py --config-path train_13.yaml &
+python ik-solver.py --config-path train_14.yaml &
+python ik-solver.py --config-path train_15.yaml &
 #python ik-solver.py --config-path train_10.yaml &
 #python ik-solver.py --config-path train_8.yaml &
 #python ik-solver.py --config-path train_10.yaml &
