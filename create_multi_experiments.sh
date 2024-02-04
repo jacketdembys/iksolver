c5e9bc2c7c12ebb9f7690e2e0ddeef08670b5604
jacketdembys <jacketdembys@yahoo.fr>
2024-02-03 20:44:20 -0600

    running experiments with another densenet architecture

--- create_multi_experiments.sh
+++ create_multi_experiments.sh
@@ -13,8 +13,8 @@
 #    python ik-solver.py --config-path "$script" &
 #done
 
-python ik-solver.py --config-path train_5.yaml &
-python ik-solver.py --config-path train_6.yaml &
+python ik-solver.py --config-path train_3.yaml &
+python ik-solver.py --config-path train_4.yaml &
 #python ik-solver.py --config-path train_3.yaml &
 #python ik-solver.py --config-path train_4.yaml &
 #python ik-solver.py --config-path train_5.yaml &
